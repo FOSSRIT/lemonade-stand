a8837636c66c1c218d85556dfe14abf71765fe45
jlew <jlew.blackout@gmail.com>
2010-07-19 18:33:23 -0400

    Removed old files

--- lemonade.activity/activity.py
+++
@@ -1,9 +0,0 @@
-from olpcgames import activity
-from gettext import gettext as _
-
-class Activity(activity.PyGameActivity):
-    """Lemonade Stand Activity"""
-    
-    game_name = 'lemonade'
-    game_title = _('Lemonade Stand')
-    game_size = None
