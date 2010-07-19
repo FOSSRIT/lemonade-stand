1958b370bf68fb25474f3d3616900b07b12d9805
jlew <jlew.blackout@gmail.com>
2010-07-19 18:56:09 -0400

    Added width/height args

--- lemonade.activity/LemonadeStand.py
+++ lemonade.activity/LemonadeStand.py
@@ -1,8 +1,19 @@
 from fortuneengine.GameEngine import GameEngine
 from LemonadeMain import LemonadeMain
 from LemonadeGui import LemonadeGui
+from optparse import OptionParser
 
-ge = GameEngine(width=640, height=480, always_draw=False)
+parser = OptionParser()
+
+parser.add_option("", "--width", dest="width", help="window width",
+                  metavar="WIDTH", default=640, type="int")
+
+parser.add_option("", "--height", dest="height", help="window height",
+                  metavar="HEIGHT", default=480, type="int")
+
+(opts, args) = parser.parse_args()
+
+ge = GameEngine(width=opts.width, height=opts.height, always_draw=False)
 ge.add_object('main', LemonadeMain() )
 ge.add_object('gui', LemonadeGui() )
 ge.start_main_loop()
