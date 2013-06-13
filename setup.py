fefc9c0d2b57b039265467c28f47b53485e7e9b8
Ryan <ras8502@rit.edu>
2013-06-13 16:17:07 -0400

    updated file structure to current standards

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 from sugar.activity import bundlebuilder
-if __name__ == "__main__":
-    bundlebuilder.start("Lemonade")
+if __name__== "__main__":
+    bundlebuilder.start()
