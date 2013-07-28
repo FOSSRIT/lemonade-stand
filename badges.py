d33eac0c24920aaaab69e636774754d457cd162a
David Wilson <daw4475@g.rit.edu>
2013-07-28 19:00:58 -0400

    Updated badges lib

--- badges.py
+++ badges.py
@@ -1,14 +1,42 @@
 from sugar.datastore import datastore
 from datetime import date
 import json
+import os
 
 
 class badges:
 
-    def __init__(self, activity):
+    def __init__(self, activity, bundle_id):
 
+        self._id = bundle_id
         ds_objects, num_objects = datastore.find({'activity': activity})
 
+        # Path for all badges
+        badge_path = os.path.expanduser('~/.local/share/badges')
+
+        # Creates a new directory for badges if one doesn't exist
+        try:
+            os.makedirs(badge_path)
+
+        # Directory already exists
+        except OSError:
+            pass
+
+        # Destination path for the activity's badges
+        dest = os.path.join(badge_path, self._id)
+        # Source path for the activity's local badges
+        source = os.path.abspath('badges/')
+
+        # Create a new directory for badges for this activity if none exist
+        try:
+            if not os.path.exists(dest):
+                os.symlink(source, dest)
+
+        # Directory already exists
+        except OSError:
+            pass
+
+        # Create a datastore object for this activity if one doesn't exist
         if not ds_objects:
             self._list = datastore.create()
             self._list.metadata['activity'] = activity
@@ -38,7 +66,8 @@ class badges:
             today = date.today()
             badge_info = {'name': name,
                           'criteria': description,
-                          'time': today.strftime("%m/%d/%y")}
+                          'time': today.strftime("%m/%d/%y"),
+                          'bundle_id': self._id}
 
             badge_json[name] = badge_info
 
